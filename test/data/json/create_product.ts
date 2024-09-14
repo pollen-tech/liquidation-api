f0d64206942501a219cdf0bdd4de87df367fd290
Rajesh <rajesh.hofo@gmail.com>
2024-09-14 21:21:32 +0800

    fixing

---
+++ test/data/json/create_product.ts
@@ -0,0 +1,40 @@
+export const create_product_req_data = {
+    "name": "Loreal facewash",
+    "pollen_sku": "90099878A",
+    "sku": "90099878A",
+    "brand_id": "43250a32-d45e-4d5e-8f82-8dc943d64c64",
+    "image": "loreal.jpeg",
+    "product_category": [
+        {
+            "category_id": "1",
+            "category_name": "cate01",
+            "sub_category": [
+                {
+                    "sub_category_id": "1",
+                    "sub_category_name": "sub_cate01",
+                    "sub_category_description": "sub_des01"
+                },
+                {
+                    "sub_category_id": "2",
+                    "sub_category_name": "sub_cate02"
+                }
+            ]
+        },
+        {
+            "category_id": "2",
+            "category_name": "cate02",
+            "sub_category": [
+                {
+                    "sub_category_id": "2",
+                    "sub_category_name": "sub_cate02",
+                    "sub_category_description": "sub_des02"
+                },
+                {
+                    "sub_category_id": "3",
+                    "sub_category_name": "sub_cate03",
+                    "sub_category_description": "sub_des03"
+                }
+            ]
+        }
+    ]
+}
