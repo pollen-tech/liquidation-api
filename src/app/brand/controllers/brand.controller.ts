ae5a5a1dfe41e1d3a585e1ea8ae03729fd91b723
Hung Tran <imhererb@gmail.com>
2024-09-17 11:30:04 +0700

    fix: Add seq_no to response + fix updateBrand API response

--- src/app/brand/controllers/brand.controller.ts
+++ src/app/brand/controllers/brand.controller.ts
@@ -10,48 +10,48 @@ import { BrandCategoryEntity } from '../repositories/brand.category.entity';
 @Controller('brand')
 @Public()
 export class BrandController {
-    constructor(private readonly brandService: BrandService) {}
+	constructor(private readonly brandService: BrandService) { }
 
-    @Post()
-    async createBrand(@Body() reqDto: NewBrandDto) {
-        return this.createApiRes(await this.brandService.createBrand(reqDto));
-    }
+	@Post()
+	async createBrand(@Body() reqDto: NewBrandDto) {
+		return this.createApiRes(await this.brandService.createBrand(reqDto));
+	}
 
-    @Get()
-    async findAllActiveBrands() {
-        return this.createApiRes(await this.brandService.findAllBrands());
-    }
+	@Get()
+	async findAllActiveBrands() {
+		return this.createApiRes(await this.brandService.findAllBrands());
+	}
 
-    @Get("/id_and_name_only")
-    async findAllActiveWithIdAndNameOnly
-    () {
-        return this.createApiRes(await this.brandService.findAllBrandsWithIdAndNameOnly());
-    }
+	@Get("/id_and_name_only")
+	async findAllActiveWithIdAndNameOnly
+		() {
+		return this.createApiRes(await this.brandService.findAllBrandsWithIdAndNameOnly());
+	}
 
-    @Get(':id')
-    async findBrandById(@Param('id') id: string) {
-        return this.createApiRes(await this.brandService.findByBrandId(id));
-    }
+	@Get(':id')
+	async findBrandById(@Param('id') id: string) {
+		return this.createApiRes(await this.brandService.findByBrandId(id));
+	}
 
-    @Get(':id/brand_category')
-    async findBrandCategoryByBrandId(@Param('id') id: string) {
-        return this.createApiRes(await this.brandService.findBrandCategorywithBrandId(id));
-    }
+	@Get(':id/brand_category')
+	async findBrandCategoryByBrandId(@Param('id') id: string) {
+		return this.createApiRes(await this.brandService.findBrandCategorywithBrandId(id));
+	}
 
-    @Put(':id')
-    updateBrand(@Param('id') id: string, @Body() updateBrandDto: NewBrandDto) {
-        return this.createApiRes(this.brandService.updateBrand(id, updateBrandDto));
-    }
+	@Put(':id')
+	async updateBrand(@Param('id') id: string, @Body() updateBrandDto: NewBrandDto) {
+		return this.createApiRes(await this.brandService.updateBrand(id, updateBrandDto));
+	}
 
-    @Delete(':id')
-    softDeleteBrand(@Param('id') id: string): Promise<void> {
-        return this.brandService.softDeleteBrand(id);
-    }
+	@Delete(':id')
+	softDeleteBrand(@Param('id') id: string): Promise<void> {
+		return this.brandService.softDeleteBrand(id);
+	}
 
-    createApiRes(data: BrandDtoRes | BrandDtoRes[] | any) {
-        return {
-            status_code: 'OK',
-            data: data,
-        };
-    }
+	createApiRes(data: BrandDtoRes | BrandDtoRes[] | any) {
+		return {
+			status_code: 'OK',
+			data: data,
+		};
+	}
 }
