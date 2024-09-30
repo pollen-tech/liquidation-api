c08ac0475d36f2032edc8d6d8ce8cb039f73f4fc
Hung Tran <imhererb@gmail.com>
2024-09-30 11:53:08 +0700

    feat: Update brand pagination

--- src/app/brand/repositories/brand.repository.ts
+++ src/app/brand/repositories/brand.repository.ts
@@ -3,10 +3,15 @@ import { CustomRepository } from '../../../database/decorators/custom-repository
 import BaseRepository from '../../../database/infrastructure/repository/base.repository';
 import { Not } from 'typeorm';
 import { Status } from '../../../common/enums/common.enum';
+import { PaginationParam } from 'src/common/pagination.entity';
 
 @CustomRepository(BrandEntity)
 export class BrandRepository extends BaseRepository<BrandEntity> {
     async findAllByActiveStatus() {
         return await this.getRepository().findBy({ status: Status.ACTIVE });
+    }
+
+    async getPaginatedBrandsByActiveStatus(paginationParam: PaginationParam) {
+        return await this.getPaginated(paginationParam, { where: { status: Status.ACTIVE } });
     }
 }
